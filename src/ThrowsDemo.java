3460445f0e263edeb6344ed50af625257eb93503
Jerin Prakash <jerinprakash892@gmail.com>
2026-02-13 10:31:45 +0530

    code added

--- src/ThrowsDemo.java
+++ src/ThrowsDemo.java
@@ -7,6 +7,36 @@ public class ThrowsDemo {
     // If n < 0, throw new IOException()
     // Else, print n
 
+	static void check(int n) throws IOException{
+
+		if(n<0){
+
+			throw new IOException();
+
+		}
+		else{
+
+			System.out.println(n);
+
+		}
+
+	}
+	public static void main(String[] args){
+
+		Scanner sc= new Scanner(System.in);
+
+		int n= sc.nextInt();
+
+		try{
+
+			check(n);
+
+		}
+		catch(IOException e){
+
+			System.out.println("Caught: IO Exception");
+		}
+	}
    
         // TODO: Read n
         
